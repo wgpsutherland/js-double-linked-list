56f500cac5fae1453d8be1750faf1114f62b8e47
wgpsutherland <wgpsutherland@gmail.com>
2015-08-10 12:26:17 +0100

    Abstract out end functions to remove repeated code

--- index.js
+++ index.js
@@ -1,15 +1,14 @@
 //==================== CONSTRUCTOR =======================//
 
-var LinkedList = module.exports = function() {
-    this._start = null;
-    this._end = null;
+var LinkedList = module.exports = function () {
+    this._start = this._end = null;
     this.length = 0;
 };
 
 //==================== PRIVATE ===========================//
 
 // Create empty node
-var makeNode = function(data) {
+var makeNode = function (data) {
     return {
         data: data,
         previous: null,
@@ -18,122 +17,111 @@ var makeNode = function(data) {
 };
 
 // Locate a node at the given index
-var findNode = function(i) {
+var findNode = function (i) {
     var item, j;
-    if(i >= this.length || i < 0 || i % 1 !== 0) {
+    if (i >= this.length || i < 0 || i % 1 !== 0) {
         item = null;
-    } else if(i < (this.length / 2)) { // first -> middle
+    } else if (i < (this.length / 2)) { // first -> middle
         item = this._start;
-        for(j = 0; j < i; j++) {
+        for (j = 0; j < i; j++) {
             item = item.next;
         }
     } else { // middle <- last
         item = this._end;
-        for(j = this.length - 1; j > i; j--) {
+        for (j = this.length - 1; j > i; j--) {
             item = item.previous;
         }
     }
     return item;
 };
 
+// Place a new node at the end of the list specified by the inputs
+var placeEnd = function (data, end, n1, n2) {
+    if (this._start) {
+        var temp = this[end];
+        this[end][n1] = makeNode(data);
+        this[end] = this[end][n1];
+        this[end][n2] = temp;
+    } else { // the list is empty
+        this._start = this._end = makeNode(data);
+    }
+    this.length++;
+};
+
+// Removes the node at the given end of the list
+var removeEnd = function (end, n1, n2) {
+    if (this[end]) { // if the list isn't empty
+        var data = this[end].data;
+        if (this[end][n1] === null) { // if the list is of length 1
+            this._end = this._start = null;
+        } else {
+            this[end] = this[end][n1];
+            this[end][n2] = null;
+        }
+        this.length--;
+    }
+    return data; // will be undefined if the list is empty
+};
+
+// Gets the given end's data if it exists
+var getEnd = function (end) {
+    if (this[end]) var x = this[end].data;
+    return x; // will be undefined if the list is empty
+};
+
 //==================== PUBLIC ===========================//
 
 // Adds the data to the front of the list
-LinkedList.prototype.unShift = function(data) {
-    if (this._start === null) { // if the list is empty
-        this._start = makeNode(data);
-        this._end = this._start;
-    } else {
-        var temp = this._start;
-        this._start.previous = makeNode(data);
-        this._start = this._start.previous;
-        this._start.next = temp;
-    }
-    this.length++;
+LinkedList.prototype.unShift = function (data) {
+    placeEnd.call(this, data, '_start', 'previous', 'next')
 };
 
 // Adds the data to the end of the list
-LinkedList.prototype.push = function(data) {
-    if (this._start === null) { // if the list is empty
-        this._start = makeNode(data);
-        this._end = this._start;
-    } else {
-        var temp = this._end;
-        this._end.next = makeNode(data);
-        this._end = this._end.next;
-        this._end.previous = temp;
-    }
-    this.length++;
+LinkedList.prototype.push = function (data) {
+    placeEnd.call(this, data, '_end', 'next', 'previous')
 };
 
 // Adds the data to the given index in the list
-LinkedList.prototype.add = function(i, data) {
-    if(i % 1 !== 0) {
+LinkedList.prototype.add = function (i, data) {
+    if (i % 1 !== 0) {
         // do nothing
-    } else if(i <= 0) {
+    } else if (i <= 0) {
         this.unShift(data);
-    } else if(i >= this.length) {
+    } else if (i >= this.length) {
         this.push(data);
     } else {
         var node = findNode.call(this, i);
-        if(node) {
+        if (node) {
             var before = node.previous;
             var after = node;
             var newNode = makeNode(data);
             newNode.previous = before;
-            before.next = newNode;
             newNode.next = after;
-            after.previous = newNode;
+            before.next = after.previous = newNode;
             this.length++;
         }
     }
 };
 
 // Removes the first item from the list
-LinkedList.prototype.shift = function() {
-    if(this._start) { // if the list isn't empty
-        var data = this._start.data;
-        if(this._start.next === null) { // if the list is of length 1
-            this._end = null;
-            this._start = null;
-        } else {
-            this._start = this._start.next;
-            this._start.previous = null;
-        }
-        this.length--;
-        return data;
-    } else {
-        return undefined;
-    }
+LinkedList.prototype.shift = function () {
+    return removeEnd.call(this, '_start', 'next', 'previous');
 };
 
 // Removes the last item from the list
-LinkedList.prototype.pop = function() {
-    if(this._end) { // if the list isn't empty
-        var data = this._end.data;
-        if(this._end.previous === null) { // if the list is of length 1
-            this._end = null;
-            this._start = null;
-        } else {
-            this._end = this._end.previous;
-            this._end.next = null;
-        }
-        this.length--;
-        return data;
-    } else {
-        return undefined;
-    }
+LinkedList.prototype.pop = function () {
+    return removeEnd.call(this, '_end', 'previous', 'next');
 };
 
 // Remove the item at the given index in the list
-LinkedList.prototype.remove = function(i) {
-    if(i === 0) {
+LinkedList.prototype.remove = function (i) {
+    if (i === 0) {
         return this.shift();
-    } else if(i === this.length - 1) {
+    } else if (i === this.length - 1) {
         return this.pop();
     } else {
         var node = findNode.call(this, i);
-        if(node) {
+        if (node) {
             var before = node.previous;
             var after = node.next;
             before.next = after;
@@ -146,27 +134,19 @@ LinkedList.prototype.remove = function(i) {
 };
 
 // Returns the item at the start of the list
-LinkedList.prototype.first = function() {
-    if(this._start) {
-        return this._start.data;
-    } else {
-        return undefined;
-    }
+LinkedList.prototype.first = function () {
+    return getEnd.call(this, '_start');
 };
 
 // Returns the item at the end of the list
-LinkedList.prototype.last = function() {
-    if(this._end) {
-        return this._end.data;
-    } else {
-        return undefined;
-    }
+LinkedList.prototype.last = function () {
+    return getEnd.call(this, '_end');
 };
 
 // Returns the element at the given index
-LinkedList.prototype.get = function(i) {
+LinkedList.prototype.get = function (i) {
     var node = findNode.call(this, i);
-    if(node) {
+    if (node) {
         return node.data;
     } else {
         return undefined;
@@ -174,11 +154,11 @@ LinkedList.prototype.get = function(i) {
 };
 
 // Returns the list in a printable format
-LinkedList.prototype.toString = function() {
+LinkedList.prototype.toString = function () {
     var item = this._start;
     var string = "";
-    for(var i = 0; i < this.length; i++) {
-        if(typeof item.data === "object") {
+    for (var i = 0; i < this.length; i++) {
+        if (typeof item.data === "object") {
             string += JSON.stringify(item.data);
         } else {
             string += item.data;
